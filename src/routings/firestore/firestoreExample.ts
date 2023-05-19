a4760dc72ac4d65a87cec53ba07c0042db6d243b
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-05-19 10:16:39 +0200

    update http options

--- src/routings/firestore/firestoreExample.ts
+++ src/routings/firestore/firestoreExample.ts
@@ -2,7 +2,7 @@ import { onDocumentCreated } from 'firebase-functions/v2/firestore'
 import { firestoreDefaultOption } from '@/routings/options'
 
 export const firestoreExample = onDocumentCreated(
-  firestoreDefaultOption('firestoreExample'),
+  firestoreDefaultOption('User/{userId}'),
   (event) => {
     console.log('firestoreExample triggered')
     try {
