c20f53688e6012e2c26f3f0935ea380e7749b758
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-05-26 12:39:59 +0200

    update service account

--- src/routings/options/authOptions.ts
+++ src/routings/options/authOptions.ts
@@ -2,8 +2,9 @@ import dotenv from 'dotenv'
 import { RuntimeOptions } from 'firebase-functions/v1'
 dotenv.config()
 
-const project = process.env.PROJECT_ID || 'skeet-example'
-const serviceAccount = `${project}@${project}.iam.gserviceaccount.com`
+const appName = process.env.SKEET_APP_NAME || 'skeet-app'
+const project = process.env.PROJECT_ID || 'skeet-app'
+const serviceAccount = `${appName}@${project}.iam.gserviceaccount.com`
 const vpcConnector = `${project}-con`
 
 export const authDefaultOption: RuntimeOptions = {
