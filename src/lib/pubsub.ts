5e01e3cd20f5468fdb8aaa6ea0e7651c26809232
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-06-14 14:52:05 +0200

    update default name

--- src/lib/pubsub.ts
+++ src/lib/pubsub.ts
@@ -1,11 +1,11 @@
 import { PubSub } from '@google-cloud/pubsub'
+import { CloudEvent } from 'firebase-functions/lib/v2/core'
+import { MessagePublishedData } from 'firebase-functions/v2/pubsub'
 
-const pubsub = new PubSub()
-
-export const pubsubPublish = async <T>(topicName: string, body: T) => {
+export const pubsubPublish = async <T>(topicName: string, json: T) => {
   try {
-    const data = Buffer.from(JSON.stringify(body))
-    const messageId = await pubsub.topic(topicName).publishMessage({ data })
+    const pubsub = new PubSub()
+    const messageId = await pubsub.topic(topicName).publishMessage({ json })
     console.log(`Message ${messageId} published.`)
     return messageId
   } catch (error) {
@@ -14,13 +14,35 @@ export const pubsubPublish = async <T>(topicName: string, body: T) => {
   }
 }
 
-export const createTopic = async (pubsub: PubSub, topicName: string) => {
+export const createTopic = async (topicName: string) => {
   try {
+    const pubsub = new PubSub()
     const [topic] = await pubsub.createTopic(topicName)
     console.log(`Topic ${topic.name} created.`)
     return topic.name
   } catch (error) {
     console.log(`createTopic: ${JSON.stringify(error)}`)
     throw new Error(JSON.stringify(error))
+  }
+}
+
+export const parsePubSubMessage = <T>(
+  event: CloudEvent<MessagePublishedData<any>>
+) => {
+  let pubsubMessage = ''
+  try {
+    pubsubMessage = Buffer.from(event.data.message.data, 'base64').toString(
+      'utf-8'
+    )
+  } catch (err) {
+    throw new Error(`Failed to decode pubsub message: ${err}`)
+  }
+
+  let pubsubObject: T
+  try {
+    pubsubObject = JSON.parse(pubsubMessage)
+    return pubsubObject
+  } catch (err) {
+    throw new Error(`Failed to parse pubsub message: ${err}`)
   }
 }
